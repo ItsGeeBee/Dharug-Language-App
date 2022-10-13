0e54ca38b5264c591fc51fc23a2344417e89f06f
Chris Newbold <chris@newboldcreative.com>
2022-10-14 07:54:19 +1100

    remove react router DOM

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Dictionary from '../components/Dictionary';

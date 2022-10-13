46ce5fda30efd8baacabbfc108d3b9b521cdb653
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-13 13:00:18 +1100

    Added db config and app.use(routes)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const routes = require('./routes');
+const db = require('./config/connection');
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +12,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
+
+// I think this should be in Routes index.js - To confirm //
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"), (err) => {
         if (err) {
@@ -15,6 +21,9 @@ app.get("/", (req, res) => {
         }
     });
 });
+
+app.use(routes);
+
 // eslint-disable-next-line no-shadow, no-unused-vars
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console

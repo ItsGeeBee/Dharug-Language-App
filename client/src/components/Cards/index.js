304dd7426c606bd73707d183884aea96fbbf65f0
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-18 21:37:32 +1100

    Wordcard and dictionary should be done. Testing needed, working on add,delete functions on Dashboard

---
+++ client/src/components/Cards/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './style.css';
+
+
+// SavedWords card component to show on dashboard 
+
+const Card = (props) => {
+
+    return (
+    <div className="WordCardContainer">
+    {props.savedWords.map((savedWord, i) => (
+  <div key={savedWord.wordId}>
+    <div className="wordHeader">
+      <h1 className="wordTitle">{savedWord.word}</h1>
+    </div>
+    <div className="wordContent">
+      <h4>{savedWord.definition}</h4>
+    </div>
+  </div>
+    ))}
+    <button
+          onClick={props.handleRemovedSaved}
+          className="btn btn-primary"
+          type="submit">
+          Delete this word from Favourties
+        </button> 
+  </div>
+)};
+
+export default Card
--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { getMe } from '../../utils/API';
+import { getMe, addWord, getAddedWord, DeleteSavedWord } from '../../utils/API';
+import { removeSavedWord,getSavedWordIds } from '../../utils/localStorage';
+import { Card } from '../../Cards/index.js'
 import Auth from '../../utils/auth';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Jumbotron, Container,  Button } from 'react-bootstrap';
 import "./style.css";
 
 
-
 const Dashboard = () => {
-  // create state for holding returned google api data
+  // create state for holding returned api data
   const [userData, setUserData] = useState({});
+  const [addedwords, setaddedWords] = useState([]);
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
@@ -37,67 +39,123 @@ const Dashboard = () => {
     getUserData();
   }, [userDataLength]);
 
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  useEffect(() => {
+    const getWordData= async () => {
+    
+      try {
+        const response = await getAddedWord()
+
+     if (!response.ok) {
+        throw new Error('something went wrong!');
+        }
+
+    const addedwords = await response.json();
+    console.log(addedwords)
+    setaddedWords(addedwords);
+  } catch (err) {
+    console.error(err);
   }
+};
+getWordData();
+  },[]);
+
+
+  useEffect(() => {
+    return () => addWord(addedwords);
+  },[addedwords]);
+
+  const [savedWordIds, setSavedWordIds] = useState(getSavedWordIds());
+  useEffect(() => {
+    return () => removeSavedWord(savedWordIds);
+  },[savedWordIds]);
+
+
+  const handleRemoveSaved = async (wordId) => {
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const response = await DeleteSavedWord(wordId, token);
+
+      if (!response.ok) {
+        throw new Error('unable to saveWord');
+      }
+      const updatedUser = await response.json();
+      setUserData(updatedUser);
+      removeSavedWord(wordId)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  // const handleAddWord = async (wordId) => {
+
+  //     const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  //     if (!token) {
+  //       return false;
+  //     }
+  //     try {
+  //       const response = await addWord(wordToAdd, token)
+        
+  //       if (!response.ok) {
+  //         throw new Error('unable to addWord');
+  //       }
+
+  //       // if word successfully saves to user's account, save book id to state
+  //       setWordList([...wordList, wordToAdd.wordId]);
+  //     } catch (err) {
+  //       console.log('Unable to setWordList')
+  //     }
+  // };
 
   return (
-    <>
-    <Jumbotron fluid className='text-light bg-dark'>
-      <Container>
+  
+    <div className='text-light bg-dark'>
+      <div>
         <h1>Dashboard</h1>
-      </Container>
-    </Jumbotron>
+      </div>
+       {/* User saved words from dictionary to show */}
+    <div>
+      <h4>
+        {userData.savedWords.length
+          ? `Viewing ${userData.savedWords.length} saved ${userData.savedWords.length === 1 ? 'word' : 'words'}:`
+          : 'You have no saved words!'}
+      </h4>
+      </div>
+        {userData.savedWords.map((word) => {
+          return (
+            <div>
+              <Card savedWords={userData}
+                    handleRemoveSaved={handleRemoveSaved} />
+            </div>
+                );
+        })}
 
-    {/* User saved words from dictionary to show */}
-    <Container>
+    {/* User added words to dictionary to show */}
+    <div className='text-light bg-dark'>
       <h4>
-        {userData.savedWords.length
-          ? `Viewing ${userData.savedWords.length} saved ${userData.savedWords.length === 1 ? 'word' : 'words'}:`
-          : 'You have no saved words!'}
+        {addedwords.length
+          ? `${userData.username}, you have added ${userData.addedwords.length === 1 ? 'word' : 'words'}:`
+          : 'Post some words and see them here!'}
       </h4>
-      <CardColumns>
-        {userData.savedWords.map((word) => {
+      </div>
+        {addedwords.map((addedword) => {
           return (
-            <Card key={word.wordId} border='dark'>
-              <Card.Body>
-                <Card.Title>{word.word}</Card.Title>
-                <Card.Text>{word.definition}</Card.Text>
-                <Button>
-                  Delete this Word!
-                </Button>
-              </Card.Body>
-            </Card>
-          );
-        })}
-      </CardColumns>
-    </Container>
-    
-{/* User added words to dictionary to show */}
-<Container>
-      <h4>
-        {userData.savedWords.length
-          ? `Viewing ${userData.savedWords.length} saved ${userData.savedWords.length === 1 ? 'word' : 'words'}:`
-          : 'You have no saved words!'}
-      </h4>
-      <CardColumns>
-        {userData.savedWords.map((word) => {
-          return (
-            <Card key={word.wordId} border='dark'>
-              <Card.Body>
-                <Card.Title>{word.word}</Card.Title>
-                <Card.Text>{word.definition}</Card.Text>
-                <Button>
-                  Delete this Word!
-                </Button>
-              </Card.Body>
-            </Card>
-          );
-        })}
-      </CardColumns>
-    </Container>
-  </>
-  );
-}
+            <div>
+              <Card addedwords={addedwords} 
+               handleDeleteWord={handleDeleteWord}/>
+              </div>
+              )})}
+       </div>       
+        );
+
+};
 // exports file
-export default Dashboard;
+export default Dashboard

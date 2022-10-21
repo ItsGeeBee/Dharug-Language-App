b2681c310df3468b9e1a98a92dea57e44f86d017
Chris Newbold <chrisnewbold@Chriss-MacBook-Pro.local>
2022-10-22 00:43:04 +1100

    debuged favourites button and added add word

--- client/src/components/WordCard/index.js
+++ client/src/components/WordCard/index.js
@@ -1,31 +1,47 @@
 import React from "react";
-
+import "./style.css";
 import { Card } from '@mui/material';
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import { Grid } from '@mui/material';
-import { box } from '@mui/material'
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { DeleteOutlined } from "@mui/icons-material";
+// import { box } from '@mui/material'
+
 export default function WordCard({ wordcards }) {
+
   return (
     <>
       {wordcards.map((wordcard) => (
-        <Grid>
+        <Grid container className="card" key={wordcard._id}>
           <Grid item xs={12} sm={6} md={3} m={3}>
-        <Card elevation="2" sx={{ maxWidth: 345 }}>
-          <CardHeader
-            key={wordcard._id}
-            title={wordcard.word}
-            subheader={wordcard.definition}
-          >
-          </CardHeader>
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {wordcard.example}
-            </Typography>
-          </CardContent>
-        </Card>
-        </Grid>
+            <Card elevation="2" sx={{ maxWidth: 345 }}>
+              <CardHeader
+                title={wordcard.word}
+                subheader={wordcard.definition}
+              ></CardHeader>
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {wordcard.example}
+                </Typography>
+              </CardContent>
+              {/* <CardActions disableSpacing>
+                <IconButton>
+                  <DeleteOutlined />
+                </IconButton>
+                <IconButton
+                  aria-label="add to favorites"
+                  onClick={() => wordcard.handleFavouriteWord(wordcard._id)}
+                >
+                  {wordcard.addFavourite}
+                  <FavoriteIcon />
+                </IconButton>
+              </CardActions> */}
+            </Card>
+          </Grid>
         </Grid>
       ))}
     </>
@@ -35,3 +51,4 @@ export default function WordCard({ wordcards }) {
 
 // export default WordCard;
 // AllFavouritesWords card component to show on dashboard
+
